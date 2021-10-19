af6cbc898743fa4552daf4e8532b969678b7bb75
Jamie Booth <Malediction99@gmail.com>
2021-10-19 14:45:17 +1100

    backend working

--- server/controllers/list-ctrl.js
+++ server/controllers/list-ctrl.js
@@ -10,11 +10,53 @@ getList = async (req, res) => {
 }
 
 createItem = (req, res) => {
+    const body = req.body
+    console.log(body)
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide an item',
+        })
+    }
 
+    const item = new Item(body)
+
+    if (!item) {
+        return res.status(400).json({ success: false, error: err })
+    }
+
+    item
+        .save()
+        .then(() => {
+            return res.status(201).json({
+                success: true,
+                id: item._id,
+                message: 'Item created!',
+            })
+        })
+        .catch(error => {
+            return res.status(400).json({
+                error,
+                message: 'Item not created!',
+            })
+        })
 }
 
-deleteItem = (req, res) => {
+deleteItem = async (req, res) => {
+    const postId = req.body.id
+    console.log(postId)
+    await Item.findOneAndDelete({ _id: postId }, (err, item) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!item) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Item not found` })
+        }
 
+        return res.status(200).json({ success: true, data: item })
+    }).clone().catch(err => console.log(err))
 }
 
 module.exports = {
